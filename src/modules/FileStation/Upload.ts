e243336dbab35464842910bb320a6238735acd0a
Kyenite-v2 <kennethmedelr@gmail.com>
2026-07-21 09:38:27 +0800

    Fixed Upload File

--- src/modules/FileStation/Upload.ts
+++ src/modules/FileStation/Upload.ts
@@ -1,33 +1,22 @@
-import { AxiosProgressEvent } from "axios";
-import { isNode } from "@/utils";
-import { createFormData, getFormDataHeaders } from "@/utils";
-import { FileStationApi, SynologyApiResponse } from "@/types";
-
-enum OverwriteEnum {
-  OVERWRITE = "overwrite",
-  SKIP = "skip",
-}
-export type UploadFileParams = {
-  path: string;
-  file: File | Blob | string | any;
-  overwrite?: OverwriteEnum;
-  create_parents?: boolean;
-  onUploadProgress?: (progress: AxiosProgressEvent) => void;
-};
-
-export type UploadFileResponse = SynologyApiResponse<{
-  blSkip: boolean;
-  file: string;
-  pid: number;
-  progress: number;
-}>;
-export async function uploadFile(params: UploadFileParams) {
+import { isNode } from "../../utils/index.js";
+import { createFormData, getFormDataHeaders } from "../../utils/index.js";
+import { FileStationApi } from "../../types/index.js";
+var OverwriteEnum;
+(function (OverwriteEnum) {
+  OverwriteEnum["OVERWRITE"] = "overwrite";
+  OverwriteEnum["SKIP"] = "skip";
+})(OverwriteEnum || (OverwriteEnum = {}));
+export async function uploadFile(params) {
   if (!this.isConnecting) {
     await this.connect();
   }
   const api = this.getApiInfoByName(FileStationApi.Upload);
-  const { path, file, overwrite = OverwriteEnum.OVERWRITE, create_parents = true } = params;
-
+  const {
+    path,
+    file,
+    overwrite = OverwriteEnum.OVERWRITE,
+    create_parents = true,
+  } = params;
   const formData = createFormData();
   formData.append("method", "upload");
   formData.append("version", String(api?.maxVersion));
@@ -35,23 +24,32 @@ export async function uploadFile(params: UploadFileParams) {
   formData.append("path", path);
   formData.append("overwrite", overwrite);
   formData.append("create_parents", String(create_parents));
-
   if (isNode) {
     let fileContent;
-    let fileName = "";
+    let fileName;
+
     if (typeof file === "string") {
-      fileName = require("path").basename(file);
-      fileContent = await require("path").readFile(file);
+      const fs = require("fs/promises");
+      const path = require("path");
+
+      fileName = path.basename(file);
+      fileContent = await fs.readFile(file);
+    } else if (file instanceof File) {
+      fileName = file.name;
+      fileContent = Buffer.from(await file.arrayBuffer());
+    } else if (Buffer.isBuffer(file)) {
+      fileName = "upload";
+      fileContent = file;
     }
+
     formData.append("file", fileContent, {
       filename: fileName,
-      contentType: "application/octet-stream", // 可根据文件类型修改
+      contentType: "application/octet-stream",
     });
   } else {
     // for browser environment
     formData.append("file", file);
   }
-
   const res = this.run(FileStationApi.Upload, {
     method: "post",
     data: formData,
